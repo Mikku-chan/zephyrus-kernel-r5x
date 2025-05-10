1a20479c23c736f83eea7e09080441078a2c0f39
mikuchan72 tt <cmiku4843@gmail.com>
2025-05-10 00:58:11 +0000

    add litle command to compiler

--- compiler.sh
+++ compiler.sh
@@ -62,7 +62,7 @@ COMPILE_START=$(date +"%s")
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee log.txt
+KCFLAGS="-O3" make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee log.txt
 
 if [[ -f out/arch/arm64/boot/Image.gz ]] ; then
 COMPILE_END=$(date +"%s")
